cd92d04633bbf229b14b9d0a2fdd1f72c854abbd
Полина Боровкова <kawaiiipolly@gmail.com>
2021-12-02 23:31:23 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace polinka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
